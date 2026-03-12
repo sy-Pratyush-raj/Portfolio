eb91d425936174af0b9bf8fdf384a87f2cc2596b
PRATYUSH RAJ <rockpratyushraj@gmail.com>
2026-03-12 10:41:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Portfolio Website Loaded");
+
+document.querySelectorAll("nav a").forEach(link => {
+link.addEventListener("click", function(e){
+e.preventDefault();
+
+const target = document.querySelector(this.getAttribute("href"));
+
+window.scrollTo({
+top: target.offsetTop - 50,
+behavior: "smooth"
+});
+});
+});
